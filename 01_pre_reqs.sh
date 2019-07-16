edae09c447ff738b9f514b2cbf7d064bf4751e4f
Alvaro Samudio <alvarosamudio@protonmail.com>
2019-07-16 10:35:56 -0500

    fix

--- 01_pre_reqs.sh
+++ 01_pre_reqs.sh
@@ -3,15 +3,7 @@
 
 # As root Execute the following command in a Terminal to get started the 
 
-apt install -y screenfetch
-apt install -y net-tools
-apt install -y openssh-server
-apt install -y debootstrap
-apt install -y squashfs-tools
-apt install -y xorriso
-apt install -y grub-pc-bin
-apt install -y grub-efi-amd64-bin
-apt install -y mtools
+apt install -y screenfetch net-tools openssh-server debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools
 
 echo " "
 echo "Edit  ./os_config.txt to customize your Distro Settings"
